fb3aa3041ad430601e80c29b67bfc2289cb1307b
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-01-22 08:46:04 +0100

    created sign-in-and-sign-up-page and sign-in component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up";
 import HatsPage from "./pages/hatspage/hatspage";
 
 const App = () => (
@@ -11,6 +12,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/shop" component={ShopPage} />
+      <Route path="/signin" component={SignInAndSignUpPage} />
       <Route path="/shop/hats" component={HatsPage} />
     </Switch>
   </div>
